9d6474dceb6b518d30dccb021bbd4e065c2fcec0
Mateusz Woda <mwoda@box.com>
2023-03-22 15:06:34 +0100

    add state validation

--- part1/app/src/main/java/com/example/boxjavasdkinandroid/MainActivity.java
+++ part1/app/src/main/java/com/example/boxjavasdkinandroid/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.boxjavasdkinandroid;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.browser.customtabs.CustomTabsIntent;
@@ -14,6 +16,7 @@ import java.net.URI;
 import java.net.URL;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
         List<String> scopes = Collections.singletonList("root_readonly");
         URI redirectUri = URI.create("oauth://boxinandroid");
         String state = UUID.randomUUID().toString();
+        MainActivity.this.getPreferences(Context.MODE_PRIVATE)
+                .edit()
+                .putString("OAuthState", state)
+                .apply();
         URL authURL = BoxAPIConnection.getAuthorizationURL(getString(R.string.client_id),
                 redirectUri, state, scopes);
         new CustomTabsIntent.Builder().build().launchUrl(this, Uri.parse(authURL.toString()));
@@ -40,6 +47,15 @@ public class MainActivity extends AppCompatActivity {
         super.onNewIntent(intent);
         if (intent != null) {
             Uri callback = intent.getData();
+            String callbackState = callback.getQueryParameter("state");
+            String savedState = MainActivity.this.getPreferences(Context.MODE_PRIVATE)
+                    .getString("OAuthState", "");
+
+            if (!Objects.equals(callbackState, savedState)) {
+                Toast.makeText(MainActivity.this, "State validation failed!", Toast.LENGTH_LONG).show();
+                return;
+            }
+
             String code = callback.getQueryParameter("code");
             Intent itemsIntent = new Intent(MainActivity.this, ItemsActivity.class);
             itemsIntent.putExtra("code", code);
